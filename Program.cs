ffdd93b201ce1df0c6655e6c51848c03332bc019
gargyogesh4218 <125199173+gargyogesh4218@users.noreply.github.com>
2023-05-04 12:52:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+    {
+
+  static  int fib(int a)
+    {
+        if (a > 1)
+            return fib(a - 1) + fib(a - 2);
+        else if (a == 1)
+            return 1;
+        else
+            return 0;
+    }
+    static void Main(string[] args)
+        {
+         Console.WriteLine("enter a number where up to be print fibonacci series :");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(fib(i));
+
+        }
+        Console.ReadLine();
+
+
+        }
+    }
+
